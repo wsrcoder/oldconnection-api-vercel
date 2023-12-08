4863dae1430ac4ce0692132fac7cb0903f2d4246
Wellington de Sousa Reis <wsr.coder@gmail.com>
2023-12-08 15:34:50 -0300

    alterações no package.json

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-const jsonServer = require("json-server")
-const server = jsonServer.create()
-const router = jsonServer.router("db/db.json")
+// JSON Server module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db/db.json");
 
-const middlewares = jsonServer.defaults()
+// Make sure to use the default middleware
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-
+server.use(middlewares);
+// Add this before server.use(router)
 server.use(
-    jsonServer.rewriter({
-        "/api/*": "/$1",
-    })
-)
-
-server.use(router)
-
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/api/*": "/$1",
+ })
+);
+server.use(router);
+// Listen to port
 server.listen(3000, () => {
-    console.log("JSON Server is running")
-})
+ console.log("JSON Server is running");
+});
 
-module.exports = server
+// Export the Server API
+module.exports = server;
